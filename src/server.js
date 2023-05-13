cd5f2e2864cd9787eeae73afab5887d5184df0e0
Zakaria Yaris <info@zakariya.me>
2023-05-13 15:26:19 +0100

    test one passes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,21 @@
-const express = require("express");
-
+const express = require('express');
 const server = express();
+
+const bodyParser = express.urlencoded({ extended: true });
+
+server.get('/', (req, res) => {
+  res.send(`
+    <!doctype html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+      </head>
+      <body>
+        <h1>Hello Express</h1>
+      </body>
+    </html>
+    `);
+});
 
 module.exports = server;
