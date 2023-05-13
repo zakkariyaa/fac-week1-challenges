5874d8d8c0a268bb29069295edd5a0f1bc009199
Zakaria Yaris <info@zakariya.me>
2023-05-13 15:27:43 +0100

    test two passes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const server = express();
 
-const bodyParser = express.urlencoded({ extended: true });
-
 server.get('/', (req, res) => {
   res.send(`
     <!doctype html>
@@ -13,6 +11,30 @@ server.get('/', (req, res) => {
       </head>
       <body>
         <h1>Hello Express</h1>
+      </body>
+    </html>
+    `);
+});
+
+server.get('/colour', (req, res) => {
+  const hexParam = req.query.hex ? '#' + req.query.hex : '#' + 'ffffff';
+
+  res.send(`
+    <!doctype html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Colour</title>
+
+        <style>
+            body {
+                background: ${hexParam};
+            }
+        </style>
+
+      </head>
+      <body>
+        <h1>Colour Page</h1>
       </body>
     </html>
     `);
